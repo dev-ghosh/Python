426b643c06ca997dc1ffda91501fe54644861f09
Your Name <dev.work616@gmail.com>
2026-05-06 22:44:58 +0530

    basic

---
+++ guessNoWhileLoop.py
@@ -0,0 +1,12 @@
+secret_number=9
+guess_count=0 #no of guesses
+guess_limit=3
+while guess_count < guess_limit:
+    guess=int(input("Guess the number: "))
+    guess_count+=1
+    if guess==secret_number:
+        print("You guessed the number")
+        break
+else:
+    print("you failed to guess the number")
+
